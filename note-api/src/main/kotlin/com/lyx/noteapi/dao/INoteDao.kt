87fb268db5ed2ba51351191a6f021883697dd7bb
农夫冰泉 <463653410@qq.com>
2017-10-17 17:32:30 +0800

    test

--- note-api/src/main/kotlin/com/lyx/noteapi/dao/INoteDao.kt
+++ note-api/src/main/kotlin/com/lyx/noteapi/dao/INoteDao.kt
@@ -1,12 +1,12 @@
 package com.lyx.noteapi.dao
 
 import com.lyx.noteapi.entity.po.Note
-import org.springframework.data.repository.CrudRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.repository.query.Param
+import org.springframework.data.repository.CrudRepository
 
 
 interface INoteDao : CrudRepository<Note, Long> {
     fun findAll(pageable: Pageable): Page<Note>
+
 }
